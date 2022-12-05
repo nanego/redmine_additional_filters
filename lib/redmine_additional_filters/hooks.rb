e74c75b0cf329846b34628373f902c09ddaa5afd
Vincent Robert <vincent.robert@nanego.com>
2022-12-05 12:43:55 +0100

    Update CI config: add latest Redmine releases

--- lib/redmine_additional_filters/hooks.rb
+++ lib/redmine_additional_filters/hooks.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-module PluginAdditionalFilters
+module RedmineAdditionalFilters
   module Hooks
     class ModelHook < Redmine::Hook::Listener
       def after_plugins_loaded(_context = {})
         require_relative 'models/issue_patch'
         require_relative 'models/query_patch' unless Rails.env.test?
         require_relative 'models/issue_query_patch'
-        require_relative 'models/issue_status_patch'
+        require_relative 'models/issue_status'
       end
     end
   end
